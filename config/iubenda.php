6005ceb7d14428184613a387fc82e6bc15f04e86
Yuriy Martini <i@ymartini.me>
2024-03-23 02:59:07 +0100

    + config

---
+++ config/iubenda.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+  'privacy_policy' => [
+    'id' => env('IUBENDA_PRIVACY_POLICY_ID'),
+  ],
+];
--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -6,4 +6,24 @@ use Illuminate\Support;
 
 class ServiceProvider extends Support\ServiceProvider
 {
+  public function boot(): void
+  {
+  }
+
+  public function register(): void
+  {
+    $this->config();
+  }
+
+  private function config(): void
+  {
+    $this->publishes(
+      [
+        __DIR__.'/../config/iubenda.php' => Support\Facades\App::configPath('iubenda.php'),
+      ],
+      ['config', 'iubenda', 'iubenda-config']
+    );
+
+    $this->mergeConfigFrom(__DIR__.'/../config/iubenda.php', 'iubenda');
+  }
 }
