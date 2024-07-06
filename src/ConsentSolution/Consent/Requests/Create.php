d22236a9b562bda5189000a497a78c9aeb081819
Yuriy Martini <i@ymartini.me>
2024-07-06 02:20:02 +0200

    request

--- src/ConsentSolution/Consent/Requests/Create.php
+++ src/ConsentSolution/Consent/Requests/Create.php
@@ -2,6 +2,7 @@
 
 namespace YMartini\Laravel\Iubenda\ConsentSolution\Consent\Requests;
 
+use YMartini\Laravel\Iubenda\ConsentSolution;
 use YMartini\Laravel\Iubenda\ConsentSolution\Consent;
 
 class Create extends Consent\Request
@@ -14,5 +15,30 @@ class Create extends Consent\Request
     protected function response(\Psr\Http\Message\ResponseInterface $response): Consent\Responses\Create
     {
         return Consent\Responses\Create::make($response);
+    }
+
+    public function subject(ConsentSolution\Subject $value): self
+    {
+        return $this->withBody('subject', $value->toArray());
+    }
+
+    public function legal_notices(array $value): self
+    {
+        return $this->withBody('legal_notices', $value);
+    }
+
+    public function proofs(array $value): self
+    {
+        return $this->withBody('proofs', $value);
+    }
+
+    public function proof_documents_ids(array $value): self
+    {
+        return $this->withBody('proof_documents_ids', $value);
+    }
+
+    public function preferences(array $value): self
+    {
+        return $this->withBody('preferences', $value);
     }
 }
