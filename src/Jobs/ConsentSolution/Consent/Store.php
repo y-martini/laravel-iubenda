7f944ff503f2080bb2b7f515fb463a92634b112b
Yuriy Martini <i@ymartini.me>
2024-07-15 04:25:05 +0200

    `iubenda.consent_solution.api_key` config is null

--- src/Jobs/ConsentSolution/Consent/Store.php
+++ src/Jobs/ConsentSolution/Consent/Store.php
@@ -5,18 +5,23 @@ namespace YMartini\Laravel\Iubenda\Jobs\ConsentSolution\Consent;
 use Illuminate\Bus;
 use Illuminate\Contracts;
 use Illuminate\Queue;
+use Illuminate\Support;
 use YMartini\Laravel\Iubenda\ConsentSolution;
 
 abstract class Store implements Contracts\Queue\ShouldQueue
 {
-  use Queue\InteractsWithQueue, Bus\Queueable;
+    use Queue\InteractsWithQueue, Bus\Queueable;
 
-  public function handle(): void
-  {
-    $this->response($this->request()->execute());
-  }
+    public function handle(): void
+    {
+        if (!Support\Facades\Config::get('iubenda.consent_solution.api_key')) {
+            Support\Facades\Log::warning('`iubenda.consent_solution.api_key` config is null');
+            return;
+        }
+        $this->response($this->request()->execute());
+    }
 
-  abstract protected function request(): ConsentSolution\Consent\Requests\Create;
+    abstract protected function request(): ConsentSolution\Consent\Requests\Create;
 
-  abstract protected function response(ConsentSolution\Consent\Responses\Create $response): void;
+    abstract protected function response(ConsentSolution\Consent\Responses\Create $response): void;
 }
